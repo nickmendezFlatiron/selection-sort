c49185d66ce5314cde3becb370693745b0bca1fd
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-04-30 20:01:19 -0400

    completed

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,11 +1,25 @@
 function selectionSort(arr) {
-  // type your code here
+//  return  arr.sort((a , b) => a - b)
+  const sortedArray= []
+  let copy = [...arr]
+  // [1 , 10 ,12 ,4, 6, 3]
+  for (let i = 1 ; i < arr.length ; i++) {
+    let min = Math.min(...copy)
+    sortedArray.push(min)
+    copy = copy.filter(value => value !== min)
+  }
+   
+
+  return sortedArray
 }
+  
+  
+
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [-1, 2, 3, 5]");
-  console.log("=>", selectionSort([3, -1, 5, 2]));
+  console.log("=>", selectionSort([1 , 10 ,12 ,4, 6, 3]));
 
   console.log("");
 
@@ -15,9 +29,13 @@ if (require.main === module) {
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+// iterate over the entire array of values
+// if the current value is less than the previous value , then push the current to a new array
+// if the current value is greater than the previous value , then push the previous value to the array
